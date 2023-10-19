018c31537d25ce78693c0b0e0f832bdd558905b8
Maitreya Gupta <95370078+MaitreyaGupta@users.noreply.github.com>
2023-10-19 12:53:40 +0530

    Update Routes1.js

--- RFuncs/Routes1.js
+++ RFuncs/Routes1.js
@@ -1,12 +1,17 @@
-const mongoose=require("mongoose")
+ const mongoose=require("mongoose")
 const UserModel=require("../models/model1")
-function  getAllData(){
-     const data1= UserModel.find({})
-     res.status(200).json({data1}) 
+const express=require("express")
+async function  getAllData(req,res){
+     const data1= await UserModel.find({})
+     res.status(200).json({app_id:"6513db506cabe444a2955a7f",
+     data1})
 }
 
 
 module.exports=getAllData
+
+
+
 
 
 
